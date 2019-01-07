6d6fe13d781eaf08f12476cad0bc7db25dabc475
Ming Cong <ming.cong188@gmail.com>
2019-01-07 11:04:12 -0800

    Initial Actor Critic Net

---
+++ network.py
@@ -0,0 +1,87 @@
+# adapted from https://github.com/ShangtongZhang/DeepRL/blob/master/deep_rl/network
+# Copyright (C) 2017 Shangtong Zhang(zhangshangtong.cpp@gmail.com)
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+
+
+DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+def layer_init(layer, w_scale=1.0):
+    nn.init.orthogonal_(layer.weight.data)
+    layer.weight.data.mul_(w_scale)
+    nn.init.constant_(layer.bias.data, 0)
+    return layer
+
+
+class FCBody(nn.Module):
+    def __init__(self, state_dim, hidden_units=(64, 64), gate=F.relu):
+        super(FCBody, self).__init__()
+        dims = (state_dim, ) + hidden_units
+        self.layers = nn.ModuleList([layer_init(nn.Linear(dim_in, dim_out)) for dim_in, dim_out in zip(dims[:-1], dims[1:])])
+        self.gate = gate
+        self.feature_dim = dims[-1]
+
+    def forward(self, x):
+        for layer in self.layers:
+            x = self.gate(layer(x))
+        return x
+
+
+class DummyBody(nn.Module):
+    def __init__(self, state_dim):
+        super(DummyBody, self).__init__()
+        self.feature_dim = state_dim
+
+    def forward(self, x):
+        return x
+
+
+class ActorCriticNet(nn.Module):
+    def __init__(self, state_dim, action_dim, phi_body, actor_body, critic_body):
+        super(ActorCriticNet, self).__init__()
+        if phi_body is None: phi_body = DummyBody(state_dim)
+        if actor_body is None: actor_body = DummyBody(phi_body.feature_dim)
+        if critic_body is None: critic_body = DummyBody(phi_body.feature_dim)
+        self.phi_body = phi_body
+        self.actor_body = actor_body
+        self.critic_body = critic_body
+        self.fc_action = layer_init(nn.Linear(actor_body.feature_dim, action_dim), 1e-3)
+        self.fc_critic = layer_init(nn.Linear(critic_body.feature_dim, 1), 1e-3)
+
+        self.actor_params = list(self.actor_body.parameters()) + list(self.fc_action.parameters())
+        self.critic_params = list(self.critic_body.parameters()) + list(self.fc_critic.parameters())
+        self.phi_params = list(self.phi_body.parameters())
+
+
+class GaussianActorCriticNet(nn.Module):
+    def __init__(self,
+                 state_dim,
+                 action_dim,
+                 phi_body=None,
+                 actor_body=None,
+                 critic_body=None):
+        super(GaussianActorCriticNet, self).__init__()
+        self.network = ActorCriticNet(state_dim, action_dim, phi_body, actor_body, critic_body)
+        self.std = nn.Parameter(torch.zeros(action_dim))
+        self.to(DEVICE)
+
+    def forward(self, obs, action=None):
+        # obs = tensor(obs)
+        phi = self.network.phi_body(obs)
+        phi_a = self.network.actor_body(phi)
+        phi_v = self.network.critic_body(phi)
+        mean = F.tanh(self.network.fc_action(phi_a))
+        v = self.network.fc_critic(phi_v)
+        dist = torch.distributions.Normal(mean, F.softplus(self.std))
+        if action is None:
+            action = dist.sample()
+        log_prob = dist.log_prob(action).sum(-1).unsqueeze(-1)
+        entropy = dist.entropy().sum(-1).unsqueeze(-1)
+        return {'a': action,
+                'log_pi_a': log_prob,
+                'ent': entropy,
+                'mean': mean,
+                'v': v}
