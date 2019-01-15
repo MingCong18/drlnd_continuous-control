907c3de93e11da2d6bb43bb7564ba735d3253624
Ming Cong <ming.cong188@gmail.com>
2019-01-15 10:11:49 -0800

    modify network for DDPG

--- network.py
+++ network.py
@@ -5,19 +5,20 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
+from utils import *
 
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 def layer_init(layer, w_scale=1.0):
     nn.init.orthogonal_(layer.weight.data)
     layer.weight.data.mul_(w_scale)
-    nn.init.constant_(layer.bias.data, 0)
+    nn.init.constant_(layer.bias.data, 0.1)
     return layer
 
 
 class FCBody(nn.Module):
-    def __init__(self, state_dim, hidden_units=(64, 64), gate=F.relu):
+    def __init__(self, state_dim, hidden_units=(128, 128), gate=F.relu):
         super(FCBody, self).__init__()
         dims = (state_dim, ) + hidden_units
         self.layers = nn.ModuleList([layer_init(nn.Linear(dim_in, dim_out)) for dim_in, dim_out in zip(dims[:-1], dims[1:])])
@@ -28,21 +29,6 @@ class FCBody(nn.Module):
         for layer in self.layers:
             x = self.gate(layer(x))
         return x
-
-
-class TwoLayerFCBodyWithAction(nn.Module):
-    def __init__(self, state_dim, action_dim, hidden_units=(64, 64), gate=F.relu):
-        super(TwoLayerFCBodyWithAction, self).__init__()
-        hidden_size1, hidden_size2 = hidden_units
-        self.fc1 = layer_init(nn.Linear(state_dim, hidden_size1))
-        self.fc2 = layer_init(nn.Linear(hidden_size1 + action_dim, hidden_size2))
-        self.gate = gate
-        self.feature_dim = hidden_size2
-
-    def forward(self, x, action):
-        x = self.gate(self.fc1(x))
-        phi = self.gate(self.fc2(torch.cat([x, action], dim=1)))
-        return phi
 
 
 class DummyBody(nn.Module):
@@ -81,7 +67,7 @@ class GaussianActorCriticNet(nn.Module):
         super(GaussianActorCriticNet, self).__init__()
         self.network = ActorCriticNet(state_dim, action_dim, phi_body, actor_body, critic_body)
         self.std = nn.Parameter(torch.zeros(action_dim))
-        self.to(DEVICE)
+        self.to(device)
 
     def forward(self, obs, action=None):
         phi = self.network.phi_body(obs)
@@ -101,30 +87,78 @@ class GaussianActorCriticNet(nn.Module):
                 'v': v}
 
 
-class DeterministicActorCriticNet(nn.Module):
-    def __init__(self,
-                 state_dim,
-                 action_dim,
-                 actor_opt_fn,
-                 critic_opt_fn,
-                 phi_body=None,
-                 actor_body=None,
-                 critic_body=None):
-        super(DeterministicActorCriticNet, self).__init__()
-        self.network = ActorCriticNet(state_dim, action_dim, phi_body, actor_body, critic_body)
-        self.actor_opt = actor_opt_fn(self.network.actor_params + self.network.phi_params)
-        self.critic_opt = critic_opt_fn(self.network.critic_params + self.network.phi_params)
-        self.to(DEVICE)
+def hidden_init(layer):
+    fan_in = layer.weight.data.size()[0]
+    lim = 1. / np.sqrt(fan_in)
+    return (-lim, lim)
 
-    def forward(self, obs):
-        action = self.actor(obs)
-        return action
 
-    def feature(self, obs):
-        return self.network.phi_body(obs)
+class Actor(nn.Module):
+    """Actor (Policy) Model."""
 
-    def actor(self, phi):
-        return F.tanh(self.network.fc_action(self.network.actor_body(phi)))
+    def __init__(self, state_size, action_size, seed, fc1_units=128, fc2_units=128):
+        """Initialize parameters and build model.
+        Params
+        ======
+            state_size (int): Dimension of each state
+            action_size (int): Dimension of each action
+            seed (int): Random seed
+            fc1_units (int): Number of nodes in first hidden layer
+            fc2_units (int): Number of nodes in second hidden layer
+        """
+        super(Actor, self).__init__()
+        self.seed = torch.manual_seed(seed)
+        self.fc1 = nn.Linear(state_size, fc1_units)
+        self.fc2 = nn.Linear(fc1_units, fc2_units)
+        self.fc3 = nn.Linear(fc2_units, action_size)
+        self.reset_parameters()
 
-    def critic(self, phi, a):
-        return self.network.fc_critic(self.network.critic_body(phi, a))
+    def reset_parameters(self):
+        self.fc1.weight.data.uniform_(*hidden_init(self.fc1))
+        self.fc2.weight.data.uniform_(*hidden_init(self.fc2))
+        self.fc3.weight.data.uniform_(-3e-3, 3e-3)
+        self.fc1.bias.data.fill_(0.1)
+        self.fc2.bias.data.fill_(0.1)
+        self.fc3.bias.data.fill_(0.1)
+
+    def forward(self, state):
+        """Build an actor (policy) network that maps states -> actions."""
+        x = F.relu(self.fc1(state))
+        x = F.relu(self.fc2(x))
+        return F.tanh(self.fc3(x))
+
+
+class Critic(nn.Module):
+    """Critic (Value) Model."""
+
+    def __init__(self, state_size, action_size, seed, fcs1_units=128, fc2_units=128):
+        """Initialize parameters and build model.
+        Params
+        ======
+            state_size (int): Dimension of each state
+            action_size (int): Dimension of each action
+            seed (int): Random seed
+            fcs1_units (int): Number of nodes in the first hidden layer
+            fc2_units (int): Number of nodes in the second hidden layer
+        """
+        super(Critic, self).__init__()
+        self.seed = torch.manual_seed(seed)
+        self.fcs1 = nn.Linear(state_size, fcs1_units)
+        self.fc2 = nn.Linear(fcs1_units+action_size, fc2_units)
+        self.fc3 = nn.Linear(fc2_units, 1)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        self.fcs1.weight.data.uniform_(*hidden_init(self.fcs1))
+        self.fc2.weight.data.uniform_(*hidden_init(self.fc2))
+        self.fc3.weight.data.uniform_(-3e-3, 3e-3)
+        self.fcs1.bias.data.fill_(0.1)
+        self.fc2.bias.data.fill_(0.1)
+        self.fc3.bias.data.fill_(0.1)
+
+    def forward(self, state, action):
+        """Build a critic (value) network that maps (state, action) pairs -> Q-values."""
+        xs = F.relu(self.fcs1(state))
+        x = torch.cat((xs, action), dim=1)
+        x = F.relu(self.fc2(x))
+        return self.fc3(x)
